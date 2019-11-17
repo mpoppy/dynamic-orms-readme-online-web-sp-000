9120f9fae46e8c3faaf72a6d434a847fcf5c3048
Marc Poppy <poppy.marc@gmail.com>
2019-11-17 16:55:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -38,7 +38,7 @@ class Song
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
 
-  def table_name_for_insert #in order to use a class method inside an instance method
+  def table_name_for_insert #in order to use a class method inside an instance method - this is to access the table name we want to insert into
     self.class.table_name
   end
 
